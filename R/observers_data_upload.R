5b3b0ab1742b7ee8145f2e83bdc8e2257efd7b2a
Jared Andrews <jared.andrews07@gmail.com>
2022-11-04 16:03:35 -0500

    Format and better document data upload observers

--- R/observers_data_upload.R
+++ R/observers_data_upload.R
@@ -1,6 +1,7 @@
 #' Define observers for data upload
 #'
-#' Define a series of observers to track additional new data being uploaded.
+#' Define a series of observers to track additional new data being uploaded. These observers
+#' also enable tabs as data is made available.
 #'
 #' @param input The Shiny input object from the server function.
 #' @param session The Shiny session object from the server function.
@@ -17,14 +18,15 @@
 #' @importFrom shinyWidgets updatePickerInput
 #' @importFrom shinyjs js
 .create_upload_observers <- function(input, session, robjects) {
+    # nocov start
     observeEvent(input$geneSummaryFiles, {
         new.data <- .gene_summ_ingress(input$geneSummaryFiles)
         robjects$gene.data <- new.data
         if (!is.null(robjects$gene.data)) {
-        js$enableTab('Gene (Overview)')
-        js$enableTab('Gene Summary Tables')
-        updateSelectizeInput(session, 'gene.sel1', choices = names(robjects$gene.data), server = TRUE)
-        updateSelectizeInput(session, 'gene.sel2', choices = names(robjects$gene.data), server = TRUE)
+            js$enableTab("Gene (Overview)")
+            js$enableTab("Gene Summary Tables")
+            updateSelectizeInput(session, "gene.sel1", choices = names(robjects$gene.data), server = TRUE)
+            updateSelectizeInput(session, "gene.sel2", choices = names(robjects$gene.data), server = TRUE)
         }
     })
 
@@ -32,11 +34,11 @@
         new.data <- .sgrna_summ_ingress(input$sgrnaSummaryFiles)
         robjects$sgrna.data <- new.data
         if (!is.null(robjects$sgrna.data)) {
-        js$enableTab('sgRNA')
-        js$enableTab('sgRNA Summary Tables')
-        updateSelectizeInput(session, 'sgrna.sel1', choices = names(robjects$sgrna.data), server = TRUE)
-        updateSelectizeInput(session, 'sgrna.sel2', choices = names(robjects$sgrna.data), server = TRUE)
-        updatePickerInput(session, 'sgrna.gene', choices = unique(c(robjects$sgrna.data[[1]]$Gene)))
+            js$enableTab("sgRNA")
+            js$enableTab("sgRNA Summary Tables")
+            updateSelectizeInput(session, "sgrna.sel1", choices = names(robjects$sgrna.data), server = TRUE)
+            updateSelectizeInput(session, "sgrna.sel2", choices = names(robjects$sgrna.data), server = TRUE)
+            updatePickerInput(session, "sgrna.gene", choices = unique(c(robjects$sgrna.data[[1]]$Gene)))
         }
     })
 
@@ -44,10 +46,10 @@
         new.data <- read.delim(input$countSummary$datapath)
         robjects$count.summary <- new.data
         if (!is.null(robjects$count.summary)) {
-        js$enableTab('QC')
-        js$enableTab('QC Table')
-        updateSelectizeInput(session, 'bip.color', choices = c('', colnames(robjects$count.summary)), server = TRUE)
-        updateSelectizeInput(session, 'bip.shape', choices = c('', colnames(robjects$count.summary)), server = TRUE)
+            js$enableTab("QC")
+            js$enableTab("QC Table")
+            updateSelectizeInput(session, "bip.color", choices = c("", colnames(robjects$count.summary)), server = TRUE)
+            updateSelectizeInput(session, "bip.shape", choices = c("", colnames(robjects$count.summary)), server = TRUE)
         }
     })
 
@@ -55,7 +57,10 @@
         new.data <- read.delim(input$countNormFile$datapath)
         robjects$norm.counts <- new.data
         if (!is.null(robjects$norm.counts)) {
-        js$enableTab('QC')
+            js$enableTab("QC")
         }
     })
+    # nocov end
+
+    invisible(NULL)
 }
